0d7982160794a070c48f8533e2eb608fc58e8d4f
Donovan Sobrero <71948474+SobreroD@users.noreply.github.com>
2020-11-02 19:24:46 -0800

    Update Detection-Uptime.ps1

--- Detection Uptime/Detection-Uptime.ps1
+++ Detection Uptime/Detection-Uptime.ps1
@@ -1,3 +1,21 @@
+
+<#PSScriptInfo
+.VERSION 1.0
+.GUID
+.AUTHOR Donovan M Sobrero
+.COMPANYNAME Networkmechanics.net
+.COPYRIGHT
+.TAGS
+.LICENSEURI
+.PROJECTURI
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES 
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES
+Version 1.0: Initial version.
+.PRIVATEDATA
+#>
 $Uptime= get-computerinfo | Select-Object OSUptime 
 if ($Uptime.OsUptime.Days -ge 7){
     Write-Output "Device has not rebootet on $($Uptime.OsUptime.Days) days, notify user to reboot"
